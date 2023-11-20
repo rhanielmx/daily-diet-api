5904118399f10cbf45ec5f757908d3b190da33ed
Rhaniel Magalhães Xavier <rhanielmag.xavier@gmail.com>
2023-11-20 13:44:09 -0300

    Primeira versão

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import fastify  from "fastify";
+import { routes } from "./routes/routes";
+
+export const app = fastify()
+
+app.register(routes)
---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify';
+
+export async function routes(app: FastifyInstance) {
+    app.get('/', async () => {
+        return {
+            message: 'Hello'
+        }
+    })
+}
